53fd7cc14f9ed66653100aae5b47315c4f27f82a
Matheus Couto] <hiiamchules@gmail.com>
2021-04-09 09:03:56 -0300

    Aula 05: módulo Task

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { UserController } from './user.controller';
 import { User } from './user.model';
 import { UserService } from './user.service';
+import { TaskModule } from './task/task.module';
 
 @Module({
-    imports: [SequelizeModule.forFeature([User])],
+    imports: [SequelizeModule.forFeature([User]), TaskModule],
     controllers: [UserController],
     providers: [UserService],
 })
